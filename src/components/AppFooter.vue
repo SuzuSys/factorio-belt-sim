9184abaec0dce1d0abce00faea180612a199e9de
SuzuSys <sigmatics@outlook.jp>
2025-01-18 08:27:57 +0900

    change footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <v-footer height="40" app> </v-footer>
+  <v-footer height="40" app>
+    <v-row justify="center">
+      <v-col class="text-center"> Factorio Belts Simulator </v-col>
+    </v-row>
+  </v-footer>
 </template>
 
 <style scoped lang="sass"></style>
