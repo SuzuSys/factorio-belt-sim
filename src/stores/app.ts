c678bf3df9694780e2b9c883630637aac45a1af5
SuzuSys <sigmatics@outlook.jp>
2025-01-18 16:16:05 +0900

    add ImageStore

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,40 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useAppStore = defineStore('app', {
+export type Grid = "grid";
+
+export type Belt =
+  | "belt-straight"
+  | "belt-left"
+  | "belt-right"
+  | "underbelt-in-straight"
+  | "underbelt-in-left"
+  | "underbelt-in-right"
+  | "underbelt-out";
+
+export const useImageStore = defineStore("imager", {
   state: () => ({
-    //
+    bitmapGrid: new Map<Grid, ImageBitmap>(),
+    bitmapBelt: new Map<
+      Belt,
+      [ImageBitmap, ImageBitmap, ImageBitmap, ImageBitmap]
+    >(),
   }),
-})
+  actions: {
+    registerBelt(
+      name: Belt,
+      images: [ImageBitmap, ImageBitmap, ImageBitmap, ImageBitmap]
+    ) {
+      this.bitmapBelt.set(name, images);
+    },
+    registerGrid(name: Grid, image: ImageBitmap) {
+      this.bitmapGrid.set(name, image);
+    },
+    getBitmapBelt(name: Belt, index: 0 | 1 | 2 | 3) {
+      return this.bitmapBelt.get(name)?.[index];
+    },
+    getBitmapGrid(name: Grid) {
+      return this.bitmapGrid.get(name);
+    },
+  },
+});
