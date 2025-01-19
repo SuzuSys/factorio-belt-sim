140243518e3fb6f333836c7b6a2bd53a4a8fd6fd
SuzuSys <sigmatics@outlook.jp>
2025-01-19 09:17:53 +0900

    update splitter between Menu and Canvas

--- src/pages/Optimizer.vue
+++ src/pages/Optimizer.vue
@@ -1,21 +1,28 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const splitterPos = ref(100);
+</script>
 
 <template>
   <v-container class="fill-height d-flex flex-row ma-0 pa-0" :fluid="true">
-    <v-sheet class="fill-height">
-      <Menu :width="100"></Menu>
+    <v-sheet class="fill-height border-e-md" style="z-index: 1">
+      <Menu :width="splitterPos"></Menu>
     </v-sheet>
-    <v-sheet
-      class="fill-height border-e-sm"
-      :width="3"
-      style="cursor: ew-resize"
-    ></v-sheet>
-    <v-sheet
-      class="fill-height border-s-sm"
-      :width="3"
-      style="cursor: ew-resize"
-    ></v-sheet>
-    <v-sheet class="fill-height flex-grow-1">
+    <v-hover class="fill-height">
+      <template v-slot:default="{ isHovering, props }">
+        <v-sheet
+          :class="[
+            'fill-height',
+            'mx-n1',
+            isHovering ? 'opacity-100' : 'opacity-0',
+          ]"
+          :width="8"
+          v-bind="props"
+          color="primary"
+          style="cursor: ew-resize; z-index: 2"
+        ></v-sheet>
+      </template>
+    </v-hover>
+    <v-sheet class="fill-height flex-grow-1 border-s-md" style="z-index: 1">
       <Canvas></Canvas>
     </v-sheet>
   </v-container>
