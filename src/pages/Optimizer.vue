f941fcbaf656dd45642d0f3bbcd3262727427578
SuzuSys <sigmatics@outlook.jp>
2025-01-19 11:01:16 +0900

    make Splitter movable

--- src/pages/Optimizer.vue
+++ src/pages/Optimizer.vue
@@ -1,5 +1,18 @@
 <script lang="ts" setup>
 const splitterPos = ref(100);
+const isForcusing = ref(false);
+function splitterMousedown(e: MouseEvent) {
+  isForcusing.value = true;
+  splitterPos.value = e.clientX - 2;
+  document.addEventListener("mousemove", resize);
+}
+function splitterMouseup(e: MouseEvent) {
+  isForcusing.value = false;
+  document.removeEventListener("mousemove", resize);
+}
+function resize(e: MouseEvent) {
+  splitterPos.value = e.clientX - 2;
+}
 </script>
 
 <template>
@@ -7,18 +20,24 @@ const splitterPos = ref(100);
     <v-sheet class="fill-height border-e-md" style="z-index: 1">
       <Menu :width="splitterPos"></Menu>
     </v-sheet>
-    <v-hover class="fill-height">
+    <v-hover open-delay="100">
       <template v-slot:default="{ isHovering, props }">
         <v-sheet
+          @mousedown="splitterMousedown"
+          @mouseup="splitterMouseup"
           :class="[
             'fill-height',
             'mx-n1',
-            isHovering ? 'opacity-100' : 'opacity-0',
+            isHovering || isForcusing ? 'opacity-100' : 'opacity-0',
           ]"
           :width="8"
           v-bind="props"
           color="primary"
-          style="cursor: ew-resize; z-index: 2"
+          style="
+            cursor: ew-resize;
+            z-index: 2;
+            transition: opacity 0.1s 0.1s ease-in-out;
+          "
         ></v-sheet>
       </template>
     </v-hover>
