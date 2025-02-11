a499a902ab91db5ff0fb881c4ed8f0a9952ab6eb
SuzuSys <sigmatics@outlook.jp>
2025-02-11 11:33:49 +0900

    add test function of params.ts

--- src/renderer/params.test.ts
+++ src/renderer/params.test.ts
@@ -1,7 +1,20 @@
 import { expect, test } from "vitest";
 import params from "@/renderer/params";
 
+// ref: ./README.md
+
 test("satisfy constraints (1)", () => {
   expect(params.minBlockSize).toBeGreaterThanOrEqual(1);
   expect(params.maxBlockSize).toBeGreaterThan(params.minBlockSize);
+});
+
+test("satisfy constraints (2)", () => {
+  expect(params.slopeA).toBeGreaterThan(0);
+  expect(params.slopeB).toBeGreaterThan(params.slopeA);
+});
+
+test("satisfy constraints (3)", () => {
+  const dif = params.maxBlockSize - params.minBlockSize;
+  expect(params.lambda).toBeGreaterThan(dif / params.slopeB);
+  expect(params.lambda).toBeLessThan(dif / params.slopeA);
 });
